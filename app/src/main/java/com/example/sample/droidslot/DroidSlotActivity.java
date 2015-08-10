478e1dc0ce523007d8523ddaf39c12eb73a53281
Minami Koga <kgmnm@gmail.com>
2015-08-10 16:20:12 +0900

    後ろ姿画像

--- app/src/main/java/com/example/sample/droidslot/DroidSlotActivity.java
+++ app/src/main/java/com/example/sample/droidslot/DroidSlotActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 
 public class DroidSlotActivity extends ActionBarActivity {
@@ -12,6 +13,8 @@ public class DroidSlotActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_droid_slot);
+        ImageView droidImage1 = (ImageView) this.findViewById(R.id.dr);
+        droidImage1.setImageResource(R.drawable.droid_back);
     }
 
     @Override
